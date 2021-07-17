8470c22564a95acfcb686e39b35fe2d32675e494
Jacob Atchley <yaaqob.atchley@gmail.com>
2021-07-16 23:59:05 -0500

    pow

--- dna/kata.cs
+++ dna/kata.cs
@@ -4,23 +4,19 @@ using NUnit.Framework;
 namespace katas.dna
 {
     /// <summary>
-    ///
-    ///Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
-
-    ///If you want to know more: http://en.wikipedia.org/wiki/DNA
-
-    ///In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You have function with one side of the DNA (string, except for Haskell); you need to get the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
-
-    ///More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
-
-    ///Example: (input: output)
-
-    ///MakeComplement("ATTGC") => "TAACG"
-    ///MakeComplement("GTAT") => "CATA"
+    ///     Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living
+    ///     organisms.
+    ///     If you want to know more: http://en.wikipedia.org/wiki/DNA
+    ///     In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You have function with one side of the DNA (string, except for Haskell);
+    ///     you need to get the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+    ///     More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+    ///     Example: (input: output)
+    ///     MakeComplement("ATTGC") => "TAACG"
+    ///     MakeComplement("GTAT") => "CATA"
     /// </summary>
     public class DnaStrand
     {
-        private static readonly Dictionary<char, char> DnaComplements = new Dictionary<char, char>()
+        private static readonly Dictionary<char, char> DnaComplements = new Dictionary<char, char>
         {
             {'A', 'T'},
             {'T', 'A'},
--- find-odd-int/katas.cs
+++ find-odd-int/katas.cs
@@ -3,12 +3,11 @@ using NUnit.Framework;
 
 namespace katas.find_odd_int
 {
-
     /// <summary>
     /// Given an array of integers, find the one that appears an odd number of times.
     // There will always be only one integer that appears an odd number of times.
     /// </summary>
-    class Kata
+    internal class Kata
     {
         public static int find_it(int[] seq)
         {
@@ -17,15 +16,15 @@ namespace katas.find_odd_int
                 .FirstOrDefault(x => x.Count() % 2 != 0)
                 .Key;
         }
-
     }
+
     [TestFixture]
     public class SolutionTest
     {
         [Test]
         public void Tests()
         {
-            Assert.AreEqual(5 , Kata.find_it ( new[] { 20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5 }));
+            Assert.AreEqual(5, Kata.find_it(new[] {20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5}));
         }
     }
 }
